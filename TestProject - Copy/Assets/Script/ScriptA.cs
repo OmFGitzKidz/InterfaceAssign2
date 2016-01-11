88489867f8c211591206d2ade7442033aad7833a
OmFGitzKidz <allenwaiker@hotmail.com>
2016-01-12 01:12:53 +0800

    Everythingdddddddddddddd

---
+++ TestProject - Copy/Assets/Script/ScriptA.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptA : MonoBehaviour {
+	public GameObject GO;
+
+	// Use this for initialization
+	void Start () {
+		GameObject[] arrayGO = GameObject.FindGameObjectsWithTag ("myTag");
+		Transform t;
+
+		foreach (GameObject go in arrayGO) {
+			t = go.GetComponent<Transform>();
+			t.position = new Vector3(0,0,0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ TestProject - Copy/Assets/Script/ScriptB.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScriptB : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+	public void DoSomething(){
+		Debug.Log ("Did something!");
+	}
+}
